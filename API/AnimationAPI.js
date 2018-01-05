93d29a1ac685c7582778126dddbc7b19364ffba2
matiwertyl2 <mateuszhazy@gmail.com>
2018-01-05 13:28:40 +0100

    all

---
+++ API/AnimationAPI.js
@@ -0,0 +1,30 @@
+
+// user API
+
+var animationData = {
+    speed : 1000,
+    maxSpeed : 100000
+}
+
+function getCanvas() {
+    return document.getElementById('canvas').getContext("2d");
+}
+
+function getCanvasWidth() {
+    return 600;
+}
+
+function getCanvasHeight() {
+    return 300;
+}
+
+function setAnimationSpeed(s) {
+    if (s>0 && s < maxSpeed) aniamtionData.speed = s;
+}
+
+///
+
+// user requirements
+
+//function drawFrame(data);
+// function initAnimation();
