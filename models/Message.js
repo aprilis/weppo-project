30af48e1fd20fd746f8bc5125011f712fbdb1763
Jarek Kwiecień <kwiecienjaro@gmail.com>
2018-01-26 23:00:22 +0100

    dodanie pokojow do chatu

--- models/Message.js
+++ models/Message.js
@@ -3,30 +3,16 @@ var mongoose = require('mongoose')
 var MessageSchema = mongoose.Schema({
     message : {type: String, required : true},
     userName : {type: String, required : true},
-    sent_at: Date
+    room : {type: String, required : true},
+    created_at: Date
 });
 
-MessageSchema.statics.getAllPromise = function () {
+MessageSchema.statics.getAllPromise = function (room) {
     return new Promise( 
         (resolve, reject) => {
-            this.find( {}, (err, messages) => {
+            this.find({room: room}, (err, messages) => {
                 if(err) reject(err);
                 resolve(messages);
-        });
-    });
-};
-
-MessageSchema.statics.saveMessagePromise = function (message) {
-    return new Promise((res, rej) => {
-        var msg = new Message({
-            userName : message.userName,
-            message : message.message,
-            created_at :  message.created_at
-        });
-
-        msg.save((err) => {
-            if (err) rej(err);
-            res(msg);
         });
     });
 };
