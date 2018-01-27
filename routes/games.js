10df7d44ce07664df9de961ff5ec2ea25ea2f41b
Jarek Kwiecień <kwiecienjaro@gmail.com>
2018-01-27 16:25:32 +0100

    szkielet strony gry

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,7 @@
 var express = require('express')
 , router = express.Router()
 , auth = require('../libs/auth');
+const languages = require('../config/languages');
 
 /**
  * GET: Redirect Homepage to login page.
@@ -12,11 +13,12 @@ router.get('/', auth.IsAuthenticated, function(req, res, next){
     });
 });
 
-router.get('/tictactoe', auth.IsAuthenticated, function(req, res, next){
-    res.render('games/tictactoe.ejs',
-    {
-        user: req.user
-    })
+router.get('/:gameId', auth.IsAuthenticated, function(req, res, next) {
+    res.render('game.ejs', {
+        user: req.user,
+        game: {},
+        languages: languages
+    });
 })
 
 console.log('Games arena');
