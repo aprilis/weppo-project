2661d5b718e99706b9f6e83f3bc376d41a670102
matiwertyl2 <mateuszhazy@gmail.com>
2018-01-29 22:49:23 +0100

    sciaganie archiwum

--- routes/instructions.js
+++ routes/instructions.js
@@ -10,5 +10,15 @@ router.get('/', auth.IsAuthenticated, function(req, res, next){
     });
 });
 
+router.post('/', auth.IsAuthenticated, (req, res, next) =>  {
+    res.redirect('instructions/archive');
+});
+
+router.get('/archive', auth.IsAuthenticated, (req, res, next) => {
+    
+    var file = 'archive.zip';
+    res.download(file); // Set disposition and send it.
+});
+
 
 module.exports = router;
